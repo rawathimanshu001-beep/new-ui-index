cd9d51a5a688e3861431b89d8cafce89306d4305
rawathimanshu001-beep <rawathimanshu001@gmail.com>
2026-08-13 23:43:34 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.HIVU_CONFIG = {
+  supabaseUrl: "https://kaiiffxgnpjsgswuhukj.supabase.co",
+  supabaseKey: "sb_publishable_C-_YpbQRpxvU3l1pcUjIHg_rmDPGRnr",
+  office: {
+    lat: 28.7041,
+    lng: 77.1025,
+    radius: 500
+  }
+};
